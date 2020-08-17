29d7dbcc217d2f17f48ef64e24dbddab9b370db9
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-08-17 14:23:59 +0530

    0.1.1: Modify the re-runing code

---
+++ app/src/main/java/com/vany/excleuploadsqlite/Dashborad.java
@@ -0,0 +1,9 @@
+package com.vany.excleuploadsqlite;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashborad extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/vany/excleuploadsqlite/MainActivity.java
+++ app/src/main/java/com/vany/excleuploadsqlite/MainActivity.java
@@ -2,13 +2,30 @@ package com.vany.excleuploadsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.vany.excleuploadsqlite.Excle.UploadExcle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button enterApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterApp = (Button) findViewById(R.id.enterApp);
+        enterApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You enter dashboard successfully. ",Toast.LENGTH_LONG).show();
+                Intent excleMainItnet= new Intent(MainActivity.this, Dashborad.class);
+                startActivity(excleMainItnet);
+            }
+        });
     }
 }
