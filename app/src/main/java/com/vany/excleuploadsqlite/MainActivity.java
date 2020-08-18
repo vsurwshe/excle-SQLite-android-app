152bfb506320e064063130cd3962eaefcbfdd8a0
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-08-18 12:18:54 +0530

    0.1.3: added the username and password fields into main page

--- app/src/main/java/com/vany/excleuploadsqlite/MainActivity.java
+++ app/src/main/java/com/vany/excleuploadsqlite/MainActivity.java
@@ -6,24 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button enterApp;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.main_usrename);
+        password = (EditText) findViewById(R.id.main_user_password);
         enterApp = (Button) findViewById(R.id.enterApp);
         enterApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You enter dashboard successfully. ",Toast.LENGTH_LONG).show();
-                Intent excleMainItnet= new Intent(MainActivity.this, Dashborad.class);
-                startActivity(excleMainItnet);
+                userLogin(username.getText().toString(), password.getText().toString());
             }
         });
+    }
+
+    public void userLogin(String username, String password) {
+        if (username.equals("Admin") && password.equals("123456")) {
+            Toast.makeText(MainActivity.this, "You enter dashboard successfully. ", Toast.LENGTH_LONG).show();
+            Intent dashboard = new Intent(MainActivity.this, Dashborad.class);
+            startActivity(dashboard);
+        } else {
+            Toast.makeText(MainActivity.this, "Sorry your correctional is wrong", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/vany/excleuploadsqlite/db/DBConstants.java
+++ app/src/main/java/com/vany/excleuploadsqlite/db/DBConstants.java
@@ -2,16 +2,20 @@ package com.vany.excleuploadsqlite.db;
 
 public class DBConstants {
     static final String USER_TABLE = "customers";
-    static final String CONTACT_ID = "contact_id";
-    static final String CONTACT_PERSON_NAME = "contact_person_name";
-    static final String CONTACT_NO = "contact_no";
-    static final String CONTACT_PHOTO = "contact_photo";
+    static final String CUSTOMER_ID = "customer_id";
+    static final String CUSTOMER_NAME = "customer_name";
+    static final String CUSTOMER_CONTACT_NO = "customer_contact_no";
+    static final String CUSTOMER_EMAIL = "customer_email";
+    static final String CUSTOMER_ADDRESS = "customer_address";
+    static final String CUSTOMER_FEEDBACK = "customer_feedback";
 
     static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + USER_TABLE + " ("
-            + CONTACT_ID + " INTEGER PRIMARY KEY,"
-            + CONTACT_PERSON_NAME + " TEXT,"
-            + CONTACT_NO + " TEXT,"
-            + CONTACT_PHOTO+" BLOB DEFAULT NULL)";
+            + CUSTOMER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + CUSTOMER_NAME + " TEXT,"
+            + CUSTOMER_CONTACT_NO + " TEXT,"
+            + CUSTOMER_EMAIL + " TEXT,"
+            + CUSTOMER_ADDRESS + " TEXT,"
+            + CUSTOMER_FEEDBACK + " TEXT)";
 
-    static final String SELECT_QUERY = "SELECT * FROM " + USER_TABLE;
+    static final String SELECT_CUSTOMER_QUERY = "SELECT * FROM " + USER_TABLE;
 }
